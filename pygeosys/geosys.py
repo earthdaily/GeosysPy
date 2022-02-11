11b857b1d86c5737ad1ea44faa006edea39b55ca
sgn <s.grevin@geosys.com>
2022-02-11 17:33:13 +0100

    Initialize project AGP

---
+++ pygeosys/geosys.py
@@ -0,0 +1,125 @@
+from oauthlib.oauth2 import LegacyApplicationClient
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import TokenExpiredError
+import requests
+import json
+import re
+from urllib.parse import urljoin
+import pandas as pd
+
+
+def renew_access_token(func):
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except TokenExpiredError:
+            self._geosys__refresh_token()
+            return func(self, *args, **kwargs)
+    return wrapper
+
+
+class geosys():
+    def __init__(self,  
+                 str_api_client_id, 
+                 str_api_client_secret, 
+                 str_api_username,
+                 str_api_password
+                ):
+        """
+        """
+        self.base_endpoint = "https://api-pp.geosys-na.net"
+        self.master_data_management_endpoint = "master-data-management/v6/seasonfields"
+        self.vts_endpoint = "https://api-pp.geosys-na.net/vegetation-time-series/v1/season-fields/values?$offset=0&$limit=2000&$count=false&SeasonField.Id=lqlvxgb&index=NDVI"
+        self.str_id_server_url = "https://identity.preprod.geosys-na.com/v2.1/connect/token"
+        self.str_api_client_id = str_api_client_id
+        self.str_api_client_secret = str_api_client_secret
+        self.str_api_username = str_api_username
+        self.str_api_password = str_api_password
+        self.token = None
+        self.__authenticate()
+    
+    
+    def __authenticate(self):
+        try :
+            oauth = OAuth2Session(client=LegacyApplicationClient(client_id=self.str_api_client_id)) 
+            self.token = oauth.fetch_token(
+                    token_url=self.str_id_server_url,
+                    username=self.str_api_username, 
+                    password=self.str_api_password, 
+                    client_id=self.str_api_client_id,
+                    client_secret=self.str_api_client_secret
+                )
+            self.token["refresh_token"] = oauth.cookies["refresh_token"]
+        except Exception as e:
+            print(e)
+    
+    
+    def __refresh_token(self):
+        client = OAuth2Session(self.str_api_client_id, token=self.token)
+        self.token = client.refresh_token(
+            self.str_id_server_url, 
+            client_id = self.str_api_client_id,
+            client_secret = self.str_api_client_secret)
+    
+    
+    def __get_matched_str_from_pattern(self, pattern, text):
+        p = re.compile(pattern)
+        return p.findall(text)[0]
+    
+    @renew_access_token
+    def get(self, url_endpoint):
+        client = OAuth2Session(self.str_api_client_id, token=self.token)
+        return client.get(url_endpoint)
+        
+    
+    @renew_access_token
+    def post(self, url_endpoint, payload):
+        client = OAuth2Session(self.str_api_client_id, token=self.token)
+        return client.post(url_endpoint, json=payload)
+    
+    
+    def __create_season_field_id(self, polygon):
+        payload = {
+            "Geometry" : polygon,
+            "Crop": {
+            "Id": "CORN"
+          },
+          "SowingDate": "2022-01-01"
+        }
+        str_url_endpoint = urljoin(self.base_endpoint, self.master_data_management_endpoint)
+
+        return self.post(str_url_endpoint, payload)
+
+    
+    def __extract_season_field_id(self, polygon):
+        
+        response = self.__create_season_field_id(polygon)
+        dict_response = response.json()
+        
+        if response.status_code == 400:
+            pattern = "\sId:\s(\w+),"
+            str_text = dict_response["errors"]["body"]["sowingDate"][0]["message"]
+            season_field_id  = self.__get_matched_str_from_pattern(pattern, str_text)
+            
+        elif response.status_code == 200 :
+            season_field_id = dict_response["id"]
+            
+        return season_field_id
+    
+
+    def get_vts_time_series(self, polygon, indicator):
+        
+        str_season_field_id = self.__extract_season_field_id(polygon)
+        self.vts_endpoint = f"/vegetation-time-series/v1/season-fields/values?$offset=0&$limit=2000&$count=false&SeasonField.Id={str_season_field_id}&index={indicator}"
+        url_vts_endpoint = urljoin(self.base_endpoint,self.vts_endpoint)
+        response = self.get(url_vts_endpoint)
+        
+        if response.status_code == 200:
+            print(response.status_code)
+            dict_response = response.json()
+            df = pd.read_json(json.dumps(dict_response))
+            df.set_index("date", inplace = True)
+            return df
+        else :
+            print(response.status_code)
+    
