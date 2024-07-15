30113a426727c4a2f1079ce2763e1f7534913139
kls <kls@geosys.com>
2024-07-15 16:48:31 -0500

    Update: tests profile

--- tests/test_unit_map_product_service.py
+++ tests/test_unit_map_product_service.py
@@ -53,28 +53,3 @@ class TestMapProductService:
             "image.date",
             "seasonField.id",
         }.issubset(set(info.columns))
-
-    @patch("geosyspy.utils.http_client.HttpClient.post")
-    def test_get_difference_map(self, get_response):
-        get_response.return_value = mock_http_response_text_content(
-            "GET",
-            load_data_from_textfile(
-                "satellite_coverage_image_references_mock_http_response"
-            ),
-        )
-        image_id_earliest = "sentinel-2-l2a%7CS2B_13SGC_20230520_0_L2A"
-        image_id_latest = "sentinel-2-l2a%7CS2B_13SGC_20230530_0_L2A"
-        field_id = "bgbrzez"
-        response = self.service.get_zipped_tiff_difference_map(
-            field_id, image_id_earliest, image_id_latest
-        )
-
-        assert response.status_code == 200, "Expected status code to be 200"
-
-        # Assert that the content type is 'image/tiff'
-        assert (
-            response.headers["Content-Type"] == "image/tiff+zip"
-        ), "Expected content type to be 'image/tiff+zip'"
-
-        # Assert that the content is not empty
-        assert response.content, "Expected non-empty response content"
